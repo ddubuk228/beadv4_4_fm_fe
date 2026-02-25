63f977180657e633ed349ab8c25768f0c7edecf7
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-25 21:00:00 +0900

    Feat: 라우터 가드 설정

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,9 @@ import ProfileEditPage from './pages/product/ProfileEditPage';
 import SellerLayout from './components/layout/SellerLayout';
 import SellerDashboardPage from './pages/member/seller/SellerDashboardPage';
 import SellerOrderDetailPage from './pages/member/seller/SellerOrderDetailPage';
+import SellerPayoutPage from './pages/member/seller/SellerPayoutPage';
+import ProtectedRoute from './components/router/ProtectedRoute';
+import SellerProtectedRoute from './components/router/SellerProtectedRoute';
 
 function App() {
   const location = useLocation();
@@ -46,6 +49,7 @@ function App() {
     else if (path.startsWith('/payment/fail')) title = 'Mossy | 결제 실패';
     else if (path.startsWith('/auth/callback')) title = 'Mossy | 로그인 처리중';
     else if (path.startsWith('/myshop/orders/')) title = 'Mossy | 판매자 주문상세';
+    else if (path.startsWith('/myshop/payout')) title = 'Mossy | 정산내역';
     else if (path.startsWith('/myshop')) title = 'Mossy | 내 상점';
 
     document.title = title;
@@ -59,22 +63,32 @@ function App() {
         <Route path="signup" element={<SignupPage />} />
         <Route path="market" element={<ProductListPage />} />
         <Route path="market/:id" element={<ProductDetailPage />} />
-        <Route path="cart" element={<CartPage />} />
-        <Route path="mypage" element={<MyPage />} />
-        <Route path="profile/edit" element={<ProfileEditPage />} />
-        <Route path="admin" element={<AdminPage />} />
-        <Route path="orders" element={<OrdersPage />} />
-        <Route path="wallet" element={<WalletPage />} />
-        <Route path="seller-request" element={<SellerRequestPage />} />
-        <Route path="payment/success" element={<PaymentSuccessPage />} />
-        <Route path="payment/fail" element={<PaymentFailPage />} />
         <Route path="auth/callback" element={<AuthCallbackPage />} />
         <Route path="signup/complete" element={<SignupCompletePage />} />
       </Route>
 
-      <Route path="/myshop" element={<SellerLayout />}>
-        <Route index element={<SellerDashboardPage />} />
-        <Route path="orders/:id" element={<SellerOrderDetailPage />} />
+      {/* Protected Routes - require login */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Layout />}>
+          <Route path="cart" element={<CartPage />} />
+          <Route path="mypage" element={<MyPage />} />
+          <Route path="profile/edit" element={<ProfileEditPage />} />
+          <Route path="admin" element={<AdminPage />} />
+          <Route path="orders" element={<OrdersPage />} />
+          <Route path="wallet" element={<WalletPage />} />
+          <Route path="seller-request" element={<SellerRequestPage />} />
+          <Route path="payment/success" element={<PaymentSuccessPage />} />
+          <Route path="payment/fail" element={<PaymentFailPage />} />
+        </Route>
+
+        {/* Seller Protected Routes - require SELLER role */}
+        <Route element={<SellerProtectedRoute />}>
+          <Route path="/myshop" element={<SellerLayout />}>
+            <Route index element={<SellerDashboardPage />} />
+            <Route path="orders/:id" element={<SellerOrderDetailPage />} />
+            <Route path="payout" element={<SellerPayoutPage />} />
+          </Route>
+        </Route>
       </Route>
     </Routes>
   );
